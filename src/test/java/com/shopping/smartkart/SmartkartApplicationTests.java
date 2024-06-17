34206e1244db419aca09a4928854c2c933673df0
A U thor <gowtham.cbe2000@gmail.com>
2024-06-17 14:33:27 +0530

    initial commit

---
+++ src/test/java/com/shopping/smartkart/SmartkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.smartkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
